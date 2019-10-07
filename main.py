425a037949a94540ce6c56cee9a027fd8bf0ed7a
Junha Park <jpark3@scu.edu>
2019-10-07 12:50:59 -0700

    Commit mid history cleanup
    Merge and cleanup flask into master
    
    Update README.md
    
    Add files via upload
    
    Add files via upload
    
    Add files via upload
    
    tied web frontend and backend

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# Survey page
+@app.route("/survey")
+def survey():
+    return render_template('survey.html')
+
 # Enable debugging when running
 if __name__ == '__main__':
     app.run(debug=True)
