59010a535ecfc8e9e4737caa42319d850f6dba42
Junha Park <jpark3@scu.edu>
2019-11-16 22:20:26 -0800

    initial commit for data sql script, PlantoGradDatabase.sql renamed to setup/schema.sql

---
+++ setup/data.sql
@@ -0,0 +1,42 @@
+INSERT INTO MajornEmphasis
+(
+  MajorName,
+  RequiredGPA,
+  RequiredCredits,
+  MinEmpClasses,
+  MinMajorClasses
+)
+VALUES
+(
+  'Computer Science, Algorithms and Complexity Emphasis',
+  2,
+  175,
+  5,
+  20
+),
+(
+  'Computer Science, Data Science Emphasis', 
+  2,
+  175,
+  5,
+  20
+),
+(
+  'Computer Science, Security Emphasis',
+  2,
+  175,
+  5,
+  20
+),
+(
+  'Computer Science, Software Emphasis',
+  2,
+  175,
+  5,
+  20
+)
+
+INSERT INTO Classes
+(
+
+)
